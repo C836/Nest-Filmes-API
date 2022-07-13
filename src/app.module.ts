f45a9e77f13bda1530e7d825ce5fc6139b56db13
C836 <gl.sepiol@gmail.com>
2022-07-12 22:43:05 -0300

    Insetindo configurações do sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MoviesModule } from './modules/movies.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [
+    MoviesModule,
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "moviesdb.db",
+      entities: ["dist/**/*.model.js"],
+      synchronize: true
+    }),
+  ],
 })
 export class AppModule {}

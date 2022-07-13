b24f5cec3e1dfc87f75608ef5eba51cf8993257b
C836 <gl.sepiol@gmail.com>
2022-07-13 14:53:44 -0300

    Implementando configuração inicial do swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
+
+  const options = new DocumentBuilder()
+    .setTitle('Movies-API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(3000);
 }

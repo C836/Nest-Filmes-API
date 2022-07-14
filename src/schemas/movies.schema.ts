ed7ca871a2c41668b6fd9e452103c453eeff8607
C836 <gl.sepiol@gmail.com>
2022-07-13 21:29:18 -0300

    Adicionando propriedades das entidades na documentação do swagger

--- src/schemas/movies.schema.ts
+++ src/schemas/movies.schema.ts
@@ -1,23 +1,52 @@
-import { IsString, MaxLength, IsInt, Min, IsDateString } from 'class-validator'
+import { IsString, MaxLength, IsInt, Min, IsDateString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class MovieSchema {
+  @IsString()
+  @MaxLength(255)
+  @ApiProperty({
+    type: String,
+    maxLength: 255,
+    description: 'Movie title',
+    example: 'Morbius',
+  })
+  title: string;
 
-    @IsString()
-    @MaxLength(255)
-    title: string;
+  @IsDateString()
+  @ApiProperty({
+    type: String,
+    pattern: '^d{4}-d{2}-d{2}$',
+    description: 'Movie release date',
+    example: '2022-03-31',
+  })
+  release: string;
 
-    @IsDateString()
-    release: string;
+  @IsInt()
+  @Min(0)
+  @ApiProperty({
+    type: Number,
+    description: 'Movie running time in minutes',
+    example: 104
+  })
+  length: number;
 
-    @IsInt()
-    @Min(0)
-    length: number;
+  @IsString()
+  @MaxLength(1024)
+  @ApiProperty({
+    type: String,
+    maxLength: 1024,
+    description: 'Movie synopsis',
+    example: 'Morbius is a 2022 American superhero film based on the Marvel Comics character of the same name[...]'
+  })
+  description: string;
 
-    @IsString()
-    @MaxLength(1024)
-    description: string;
-
-    @IsString()
-    @MaxLength(255)
-    director: string;
+  @IsString()
+  @MaxLength(255)
+  @ApiProperty({
+    type: String,
+    maxLength: 255,
+    description: 'Movie director name',
+    example: 'Daniel Espinosa'
+  })
+  director: string;
 }

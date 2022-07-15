907c01029038bbcd73c0255b2844ca21809a2c7f
C836 <gl.sepiol@gmail.com>
2022-07-14 21:02:14 -0300

    Implementando testes da rota GetAll

--- src/movies/movies.controller.spec.ts
+++ src/movies/movies.controller.spec.ts
@@ -1,7 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesController } from './movies.controller';
-import { MovieModel } from './movies.model';
 import { MovieService } from './movies.service';
+
+import { mockMovieList, mockMovieEntity, mockMovieUpdatedEntity } from './schemas/movie.mocks'
+
 describe('TodoController', () => {
   let movieController: MoviesController;
   let movieService: MovieService;
@@ -13,7 +15,7 @@ describe('TodoController', () => {
         {
           provide: MovieService,
           useValue: {
-            GetAll: jest.fn().mockResolvedValue(MovieModel),
+            GetAll: jest.fn().mockResolvedValue(mockMovieList)
           },
         },
       ],
@@ -21,5 +23,20 @@ describe('TodoController', () => {
 
     movieController = module.get<MoviesController>(MoviesController);
     movieService = module.get<MovieService>(MovieService);
+  });
+
+  it('should be defined', () => {
+    expect(MoviesController).toBeDefined();
+    expect(MovieService).toBeDefined();
+  });
+
+  describe('GetAll', () => {
+    it('return should be equal to movie model', async () => {
+      const result = await movieController.GetAll();
+
+      expect(result).toEqual(mockMovieList);
+      expect(typeof result).toEqual('object');
+      expect(movieService.GetAll).toHaveBeenCalledTimes(1);
+    });
   });
 });
